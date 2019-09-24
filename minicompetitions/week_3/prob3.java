c4498a3430ba5047efc992522e32e8f1f2757e91
Albert Robin Moriarty <19686802+armoriarty@users.noreply.github.com>
2019-09-24 16:04:45 -0400

    Added a working version of the week 3 code

--- minicompetitions/week_3/prob3.java
+++ minicompetitions/week_3/prob3.java
@@ -6,30 +6,24 @@ public class prob3 {
     private static Scanner in;
     private static PrintWriter out;
     private static String lastString;
-    private static boolean stopFlag;
 
     public static void main (String[] args) throws Exception {
         lastString = "";
         in = new Scanner (new File ("prob3.in"));
         out = new PrintWriter ("prob3.out");
-        String S = in.nextLine();
+        String inputString = in.nextLine();
         int count = 1;
-        while(!S.equals("")){
-            stopFlag = false;
+        while(!inputString.equals("")){
             String intString = in.nextLine();
             int stringLength = Integer.parseInt(intString);
-            char[] SC = S.toCharArray ();
-            Arrays.sort (SC);
-            String SS = new String (SC);
-            out.print("Case ");
-            out.print( count);
-            out.print(": The alphebetical substring of ");
-            out.print(SS);
-            out.println(" are: ");
-            permute("", stringLength, SS);
+            char[] inputArray = inputString.toCharArray();
+            Arrays.sort (inputArray);
+            String sortedInput = new String (inputArray);
+            out.printf("Case: %d: The alphebetical substrings of %s are:\n", count, sortedInput);
+            permute("", stringLength, sortedInput);
             out.println();
             count++;
-            S = in.nextLine();
+            inputString = in.nextLine();
 
         }
         in.close ();
@@ -37,26 +31,29 @@ public class prob3 {
     }
 
     public static void permute(String prefix, int stringLength, String candidates){
-        if(prefix.length() > stringLength){
-            return;
-        }
-        if(prefix.length() == stringLength ){
-            if(lastString.equals(prefix)){
-                stopFlag = true;
-                return;
-            }
-            out.println(prefix);
-            System.out.println(prefix);
-            lastString = prefix;
+        if(stringLength == 0){
+            out.printf("%s\n", prefix);
         } else {
-            for(int i = 0; i < candidates.length(); i++){
-                if(stopFlag){
-                    return;
+            for(int i = 0; i <= candidates.length() - stringLength; i++)
+                if( i == 0 || candidates.charAt(i) > candidates.charAt(i - 1)){
+                    String newPrefix = prefix + candidates.charAt(i);
+                    String newCandidates = candidates.substring(i + 1);
+                    permute(newPrefix, stringLength - 1, newCandidates);
                 }
-                String newPrefix = prefix + candidates.charAt(i);
-                String newcandidates = candidates.substring(i + 1);
-                permute(newPrefix, stringLength, newcandidates);
             }
         }
-    }
+//        if(prefix.length() + candidates.length() < stringLength){
+//            return;
+//        }
+//        if(prefix.length() == stringLength && !prefix.equals(lastString) ){
+//            out.println(prefix);
+//            lastString = prefix;
+//        } else {
+//            for(int i = 0; i < candidates.length() - stringLength; i++){
+//                String newPrefix = prefix + candidates.charAt(i);
+//                String newcandidates = candidates.substring(i + 1);
+//                permute(newPrefix, stringLength, newcandidates);
+//            }
+//        }
+//    }
 }
