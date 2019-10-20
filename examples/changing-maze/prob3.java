9954d7247ba09bee10d2bfc79fcc0e78006a63bb
Albert Robin Moriarty <19686802+armoriarty@users.noreply.github.com>
2019-10-19 22:32:32 -0400

    Added Solution and Reference for Changing Maze
    The Changing Maze problem is a basic maze solving challenge.
    I added
    - a solution
    - the pdf for the challenge
    - a sample input file

---
+++ examples/changing-maze/prob3.java
@@ -0,0 +1,127 @@
+import java.io.*;
+import java.util.*;
+
+public class prob3 {
+
+    private static Scanner in;
+    private static PrintWriter out;
+    private static int[][][] maze;
+    private static int rows, columns, levels;
+    private static LinkedList<Position> futureMoves;
+
+    private static class Position {
+        public static int row;
+        public static int column;
+        public static int level;
+
+        public Position(int aRow, int aColumn, int aLevel){
+            row = aRow;
+            column = aColumn;
+            level = aLevel;
+        }
+    }
+
+    public static void main (String[] args) throws Exception {
+        in = new Scanner (new File ("prob3.in"));
+        out = new PrintWriter ("prob3.out");
+
+        while(in.hasNext()){
+            rows = in.nextInt();
+            columns = in.nextInt();
+            levels = in.nextInt();
+            if(rows == 0){
+                return;
+            }
+            //System.out.printf("%d %d %d\n", rows, columns, levels);
+            in.nextLine();
+
+            maze = new int[rows + 2][columns + 2][levels];
+            for(int i = 0; i < rows + 2; i++){
+                for(int j = 0; j < columns + 2; j++){
+                    for(int k = 0; k < levels; k++){
+                        maze[i][j][k] = 1;
+                    }
+                }
+            }
+            printMaze();
+            System.out.flush();
+
+            for(int k = 0; k < levels; k++){
+                for(int i = 1 ; i <= rows; i++){
+                    String aRow = in.nextLine();
+                    for(int j = 1; j <= columns; j++){
+                        maze[i][j][k] = '0' - aRow.charAt(j - 1);
+                    }
+                }
+                in.nextLine();
+            }
+
+            maze[1][1][0] = 1;
+            printMaze();
+            System.out.flush();
+            process();
+        }
+
+
+
+        in.close ();
+        out.close ();
+    }
+
+    public static void process(){
+
+        futureMoves = new LinkedList<>();
+        futureMoves.add(new Position(1, 1, 0));
+
+        while(!futureMoves.isEmpty()){
+            Position now = futureMoves.remove();
+            System.out.printf("%d %d %d\n\n", now.row, now.column, now.level);
+            printMaze();
+            System.out.println();
+            System.out.flush();
+
+            if(now.row == rows && now.column == columns){
+                out.printf("Luke and Leia can escape in %d steps.\n", maze[now.row][now.column][now.level] - 1);
+                out.flush();
+                return;
+            }
+
+            if(maze[now.row][now.column][(now.level + 1) % levels] == 0){
+                maze[now.row][now.column][(now.level + 1) % levels] = maze[now.row][now.column][now.level] + 1;
+                futureMoves.add(new Position(now.row, now.column, ((now.level + 1) % levels)));
+            }
+            if(maze[now.row + 1][now.column][(now.level + 1) % levels] == 0){
+                maze[now.row + 1][now.column][(now.level + 1) % levels] = maze[now.row][now.column][now.level] + 1;
+                futureMoves.add(new Position(now.row + 1, now.column, ((now.level + 1) % levels)));
+            }
+            if(maze[now.row - 1][now.column][(now.level + 1) % levels] == 0){
+                maze[now.row - 1][now.column][(now.level + 1) % levels] = maze[now.row][now.column][now.level] + 1;
+                futureMoves.add(new Position(now.row - 1, now.column, ((now.level + 1) % levels)));
+            }
+            if(maze[now.row][now.column + 1][(now.level + 1) % levels] == 0){
+                maze[now.row][now.column + 1][(now.level + 1) % levels] = maze[now.row][now.column][now.level] + 1;
+                futureMoves.add(new Position(now.row, now.column + 1, ((now.level + 1) % levels)));
+            }
+            if(maze[now.row][now.column - 1][(now.level + 1) % levels] == 0){
+                maze[now.row][now.column - 1][(now.level + 1) % levels] = maze[now.row][now.column][now.level] + 1;
+                futureMoves.add(new Position(now.row, now.column - 1, ((now.level + 1) % levels)));
+            }
+        }
+        out.printf("Luke and leia cannot escape.\n");
+        out.flush();
+    }
+
+
+    public static void printMaze(){
+        for(int k = 0; k < levels; k++){
+            for(int i = 0; i < rows + 2; i++){
+                for(int j = 0; j < columns + 2; j++){
+                    System.out.printf("%d ", maze[i][j][k]);
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
